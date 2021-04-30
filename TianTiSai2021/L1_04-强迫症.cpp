d5aa59f000a1a5d020ade2feb9143e7c2302e705
HailayLin <hailay@qq.com>
2021-04-30 20:17:42 +0800

    做今年天梯赛的题目，手疼，还好没去…………打三个小时代码手肯定废掉了

---
+++ TianTiSai2021/L1_04-强迫症.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file L1_04-强迫症.cpp
+ * @author HailayLin (hailay@qq.com)
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021  Hailay.site & Xihe.ai
+ * 7-4 强迫症 (10 分)
+小强在统计一个小区里居民的出生年月，但是发现大家填写的生日格式不统一，例如有的人写 199808，有的人只写 9808。有强迫症的小强请你写个程序，把所有人的出生年月都整理成 年年年年-月月 格式。对于那些只写了年份后两位的信息，我们默认小于 22 都是 20 开头的，其他都是 19 开头的。
+
+输入格式：
+输入在一行中给出一个出生年月，为一个 6 位或者 4 位数，题目保证是 1000 年 1 月到 2021 年 12 月之间的合法年月。
+
+输出格式：
+在一行中按标准格式 年年年年-月月 将输入的信息整理输出。
+
+输入样例 1：
+9808
+输出样例 1：
+1998-08
+输入样例 2：
+0510
+输出样例 2：
+2005-10
+输入样例 3：
+196711
+输出样例 3：
+1967-11
+ */
+
+//一开始思路：截取数字int类型，判断后cout输出
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int tempyyyymm;
+    int yy, mm;
+    cin >> tempyyyymm;
+    //if (isYYYYMM(tempyyyymm)){
+        mm = tempyyyymm % 100;
+        yy = (tempyyyymm - mm)/100;
+    //}
+    cout << "yy=" << yy << "mm=" << mm << endl;
+}
