b969a4223a07aa08e9e3091ad72d321b98e67a31
HailayLin <hailay@qq.com>
2021-05-13 20:40:15 +0800

    刘辉提出来的问题……单步调试很清楚每一步干了什么

--- debug/debug-file.cpp
+++ debug/debug-file.cpp
@@ -1,28 +1,21 @@
 /**
+ * @file debug-file.cpp
+ * @author HailayLin (hailay@qq.com)
+ * @version 0.1
+ * @date 2021-05-12
  * 
- *
- * int arr[5]={1,2,3,4,5};
- *  *(*(&arr+1)-1)  
+ * @copyright Copyright (c) 2021  Hailay.site & Xihe.ai
+ * 
  */
-
-#include<iostream>
-using namespace std;
-
+#include<stdio.h>
+#include<string.h>
+//#include<iostream>
+//#include<cstring>
+//using namespace std;
 int main(){
-    int arr[5]={1,2,3,4,5};
-    int arr2[5]={6,7,8,9,0};
-    cout << "arr=\t"<< arr << endl;
-    cout << "arr+1=\t" << arr+1 << endl;
-    cout << "arr2=\t" << arr2 << endl;
-    cout << "*(arr+1)=\t" << *(arr+1) << endl;
-    cout << "&arr=\t" << &arr << endl;
-    cout << "&arr+1\t" << &arr+1 << endl;
-    cout << "*(&arr+1)=\t" << *(&arr+1) << endl;
-    cout << "*(*(&arr+1))=\t" << *(*(&arr+1)) << endl;
-    //cout << *(*(&arr[0]+1)-1) << endl; //"*" 的操作数必须是指针，但它具有类型 "int"
-    //cout << *(*(arr+1)-1) << endl; //"*" 的操作数必须是指针，但它具有类型 "int"
-    cout << "*(*(&arr+1)-1)=\t" << *(*(&arr+1)-1) << endl;
-    cout << "*(*(&arr+1)+1)=\t" << *(*(&arr+1)+1) << endl;
-    cout << "*(*(&arr+1)+2)=\t" << *(*(&arr+1)+2) << endl;
+    char arr[2][4];
+    strcpy(*arr,"you");
+        strcpy(arr[1],"me");
+    arr[0][3]='&';
+    printf("%s\n",*arr);
 }
-
