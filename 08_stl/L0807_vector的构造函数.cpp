4c09d9710468ca58c9192d03827e59a36ef115af
HailayLin <hailay@qq.com>
2021-05-22 21:41:36 +0800

    奇怪的模板报错，把vector函数输入搞成模板

---
+++ 08_stl/L0807_vector的构造函数.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file L0807_vector的构造函数.cpp
+ * @author HailayLin (hailay@qq.com)
+ * @version 0.1
+ * @date 2021-05-22
+ * 
+ * @copyright Copyright (c) 2021  Hailay.site & Xihe.ai
+ * 题号：L0807        题目:vector的构造函数        得分：0  
+  
+作业提交截止时间：2021/7/15 0:00:00	 
+ 	
+题目内容：
+练习书上例8.7，其中输出时以逗号间隔。
+另：输出vector容器时请尽量使用函数完成。
+ */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+//貌似不能用模板？
+template<typename T>
+void vprint(vector<T> a){
+    template<typename T>    //a template declaration cannot appear at block scope
+    vector<T>::iterator i;
+    for(i = a.begin(); i!=a.end(); i++){    //'i' was not declared in this scope
+        cout << i << ",";
+    }
+    printf("\n");
+}
+
+int main(){
+    vector<int> nu(5,99);
+    vprintf(nu);    //cannot convert 'std::vector<int>' to 'const char*'
+}
