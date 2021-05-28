5b03d471304c055bfcc37ca1085bea9bc847720d
HailayLin <hailay@qq.com>
2021-05-28 14:16:02 +0800

    多文件编译？OK？

--- 02_basic/0202-c+cpp.cpp
+++ 02_basic/0202-c+cpp.cpp
@@ -7,6 +7,9 @@
  * @copyright Copyright (c) 2021  Hailay.site & Xihe.ai
  * c和cpp混合编译
  * c cpp 混合编译用gcc不行，用g++ok
+ * 可编译的指令
+ * gcc -Wall .\0202-c+cpp.cpp .\0202-myfun.c .\0202-myfun.h -lstdc++ -o 0202.exe ; .\0202.exe
+ * g++ 或者 c++ Wall .\0202-c+cpp.cpp .\0202-myfun.c .\0202-myfun.h -o 0202.exe ; .\0202.exe
  */
 
 #include <iostream>
--- 02_basic/0202-myfun.h
+++ 02_basic/0202-myfun.h
@@ -8,4 +8,13 @@
  * 
  */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void display();
+
+#ifdef __cplusplus
+}
+#endif
+
