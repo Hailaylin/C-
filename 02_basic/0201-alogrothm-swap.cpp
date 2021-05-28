59f07cc513d6dde816be0857faa5f283940211dd
HailayLin <hailay@qq.com>
2021-05-28 13:39:45 +0800

    alogrithm 里面有函数 swap()

---
+++ 02_basic/0201-alogrothm-swap.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file 0201-alogrothm-swap.cpp
+ * @author HailayLin (hailay@qq.com)
+ * @version 0.1
+ * @date 2021-05-28
+ * 
+ * @copyright Copyright (c) 2021  Hailay.site & Xihe.ai
+ * alogrithm 里面有函数 swap()
+ */
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int a = 3, b = 6;
+    cout << "a=" << a << "\tb=" << b << endl;
+    swap(a,b);
+    cout << "a=" << a << "\tb=" << b << endl;
+}
